ac0fa9670c47a11d03cdaf054af8cc3aea4501c4
himanshumahaur <himanshumahaur@outlook.com>
2025-04-18 08:46:53 +0000

    Added Telescope

--- init.lua
+++ init.lua
@@ -1,5 +1,7 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " " 
+
 require("lazy").setup("plugins")
 require("zerefco")
--- lua/zerefco.lua
+++ lua/zerefco.lua
@@ -1,2 +1,7 @@
 vim.keymap.set('n', '<C-t>', ':NvimTreeFocus<CR>')
+
+vim.keymap.set('n', '<leader>ff', ':Telescope find_files<CR>')
+vim.keymap.set('n', '<leader>fg', ':Telescope live_grep<CR>')
+vim.keymap.set('n', '<leader>fb', ':Telescope buffers<CR>')
+
 vim.cmd[[colorscheme dracula]]
